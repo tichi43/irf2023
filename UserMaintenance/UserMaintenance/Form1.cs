e4400684e5e15f40d0547e00376be52f9e7b651b
rqh3lj <rqh3lj@SZA0025882>
2023-03-07 09:31:43 +0100

    negyedik

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 using UserMaintenance.Entities;
 
@@ -19,6 +14,7 @@ namespace UserMaintenance
             InitializeComponent();
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.WriteOut;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -28,6 +24,22 @@ namespace UserMaintenance
         {
             var u = new User() { FullName = textBox1.Text };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".csv";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName);
+                foreach (var item in users)
+                {
+                    streamWriter.WriteLine(item.ID + "," + item.FullName);
+                }
+                streamWriter.Close();
+            }
+
         }
     }
 }
